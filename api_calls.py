4e1feff56894bc693cc077721513a2fdfd9bc765
Harry Beesley-Gilman <142692576+HarryBeesley-Gilman@users.noreply.github.com>
2023-09-02 17:36:00 -0400

    Update and rename survey_methods.py to api_calls.py

--- api_calls.py
+++ api_calls.py
@@ -1,23 +1,50 @@
+# Harry Beesley-Gilman
+# May 21st, 2023
+# Call OpenAI and PaLM API's
+
 from bardapi.core import Bard
 from dotenv import load_dotenv
-load_dotenv("api_keys.env")
 import os 
 import openai
+from bardapi import BardCookies
+from Bard import Chatbot
+import google.generativeai as palm
+import random
 
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-bard_api_key = os.getenv("BARD_API_KEY")
 
-def call_bard(query, api_key):
-	BARD_API_KEY = api_key
-	bard = Bard(BARD_API_KEY)
-	answer = bard.get_answer(query)
-	return (answer['content'])
+
+def call_bard(query, api_key, temperature_in):
+
+
+
+    palm.configure(api_key=api_key)
+
+
+    temp = temperature_in
+
+
+    print("answering query:")
+    print(query)
+
+    completion = palm.generate_text(
+        model= "models/text-bison-001",
+        prompt= query,
+        temperature=temp,
+        # The maximum length of the response (in tokens)
+        max_output_tokens=1000,
+    )
+
+
+    print(completion.result)
+  
+
+    return completion.result
+
 
 def call_ChatGPT(question, temperature, max_retries):
     for i in range(max_retries):
         try:
-            print("trying")
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=[
@@ -28,10 +55,10 @@ def call_ChatGPT(question, temperature, max_retries):
             )
             
             answer = response['choices'][0]['message']['content'].strip()
+
+            return answer
+
             
-            # Check if the answer contains '1.' and '2.'
-            if '1.' in answer.lower() and '2.' in answer.lower():
-                return answer
 
         except openai.error.RateLimitError as e:
             print("Rate limit error, retrying... ({}/{})".format(i+1, max_retries))
@@ -39,7 +66,8 @@ def call_ChatGPT(question, temperature, max_retries):
                 sleep(20)  # Wait for 20 seconds before trying again
             else:
                 raise
-
+    print("exhausted errors")
+    return None
 
 
 
